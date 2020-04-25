71285937189e3c4cd7e504675694e4f8551028f7
Raj <sharaj@deloitte.com>
2020-04-26 01:32:49 +0530

    AEM project Created

---
+++ emailworkflow/bundle/src/main/java/com/aem/workflowstep/HelloService.java
@@ -0,0 +1,13 @@
+package com.aem.workflowstep;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
